7937dbee7ba8cf2ff2837653a1d7fd7f827ace8c
Eugene Prystupa <prystupa@sqlapi.com>
2012-09-30 12:58:50 -0400

    stateless and stateful sessions

--- src/main/java/com/prystupa/Room.java
+++ src/main/java/com/prystupa/Room.java
@@ -9,6 +9,10 @@ package com.prystupa;
 public class Room {
     private String name;
 
+    public Room(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/com/prystupa/FireAlarmTest.java
+++ src/test/java/com/prystupa/FireAlarmTest.java
@@ -1,18 +1,21 @@
 package com.prystupa;
 
+import org.drools.ClassObjectFilter;
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.StatelessKnowledgeSession;
+import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -24,28 +27,93 @@ import static org.junit.Assert.assertTrue;
  */
 public class FireAlarmTest {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Room room;
+    private Fire fire;
+    private Sprinkler sprinkler;
+    private StatelessKnowledgeSession session;
+    private StatefulKnowledgeSession statefulSession;
 
-    @Test
-    public void testFireAlarm() {
-
-        logger.info("Works...");
+    @Before
+    public void setUp() {
 
         KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         knowledgeBuilder.add(ResourceFactory.newClassPathResource("fireAlarm.drl", App.class), ResourceType.DRL);
 
-        assertFalse(knowledgeBuilder.hasErrors());
+        assertFalse(knowledgeBuilder.getErrors().toString(), knowledgeBuilder.hasErrors());
 
         KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
         knowledgeBase.addKnowledgePackages(knowledgeBuilder.getKnowledgePackages());
 
-        StatelessKnowledgeSession session = knowledgeBase.newStatelessKnowledgeSession();
+        room = new Room("office");
+        fire = new Fire(room);
+        sprinkler = new Sprinkler(room, false);
 
-        Room room = new Room();
-        Fire fire = new Fire(room);
-        Sprinkler sprinkler = new Sprinkler(room, false);
+        session = knowledgeBase.newStatelessKnowledgeSession();
+        statefulSession = knowledgeBase.newStatefulKnowledgeSession();
+    }
+
+    @Test
+    public void testSprinklerTurnsOnWhenFire() {
+
+        // Arrange
+
+        // Act
         session.execute(Arrays.asList(room, fire, sprinkler));
 
+        // Assert
         assertTrue(sprinkler.isOn());
+    }
+
+    @Test
+    public void testSprinklerTurnsOffWhenFireGone() {
+
+        // Arrange
+        session.execute(Arrays.asList(room, fire, sprinkler));
+
+        // Act
+        session.execute(Arrays.asList(room, sprinkler));
+
+        // Assert
+        assertFalse(sprinkler.isOn());
+    }
+
+    @Test
+    public void testFireAlarmCycle() {
+
+        // Setup
+        Room kitchen = new Room("kitchen");
+        Room bedroom = new Room("bedroom");
+        Room office = new Room("office");
+
+        for (Room room : Arrays.asList(kitchen, bedroom, office)) {
+            Sprinkler sprinkler = new Sprinkler(room, false);
+
+            statefulSession.insert(room);
+            statefulSession.insert(sprinkler);
+        }
+
+        statefulSession.fireAllRules();
+
+        // Alarm should not be active
+        Collection alarms = statefulSession.getFactHandles(new ClassObjectFilter(Alarm.class));
+        assertEquals(0, alarms.size());
+
+        // Set office on fire
+        Fire officeFire = new Fire(office);
+        statefulSession.insert(officeFire);
+        statefulSession.fireAllRules();
+        assertEquals("Alarm should have activated", 1, statefulSession.getFactHandles(new ClassObjectFilter(Alarm.class)).size());
+
+        // Set bedroom on fire
+        Fire bedroomFire = new Fire(bedroom);
+        statefulSession.insert(bedroomFire);
+        statefulSession.fireAllRules();
+        assertEquals("Alarm should still be activated", 1, statefulSession.getFactHandles(new ClassObjectFilter(Alarm.class)).size());
+
+        // Combat the fire
+        statefulSession.retract(statefulSession.getFactHandle(officeFire));
+        statefulSession.retract(statefulSession.getFactHandle(bedroomFire));
+        statefulSession.fireAllRules();
+        assertEquals("Alarm should have deactivated", 0, statefulSession.getFactHandles(new ClassObjectFilter(Alarm.class)).size());
     }
 }
